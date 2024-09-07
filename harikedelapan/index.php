d97678b4ee74b0c3b765f82490fbae9f206a35b2
nadyakaputri <nadyaksp80@gmail.com>
2024-09-07 12:46:22 +0700

    tugasdasar

---
+++ harikedelapan/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get dan Post</title>
+</head>
+<body>
+    <h1>Mengirim data dengan Method GET</h1>
+    <a href="link1.php?
+    nama=Nadya">Link 1</a>
+
+
+    <hr>
+    <h1>Mengirim data dengan method POST</h1>
+    <form action="link2.php"
+    method="post">
+<input type="text"
+name="kelas" id="kelas">
+<button type="submit">kirim</button>
+</form>
+</body>
+</html>
